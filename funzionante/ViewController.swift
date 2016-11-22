7a0c932a7b7c2ee3db3a5524e382d76f1bc170a0
Mario Regeni <Mario@retinas-macbook.homenet.telecomitalia.it>
2016-11-22 13:58:08 +0100

    Initial Commit

---
+++ funzionante/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  funzionante
+//
+//  Created by Mario Regeni on 22/11/16.
+//  Copyright © 2016 Mario Regeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
